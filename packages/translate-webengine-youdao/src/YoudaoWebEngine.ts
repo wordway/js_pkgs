086ec06a7d2020898738d3cbb828248c2c14a551
LiJianying <lijy91@foxmail.com>
2020-11-17 20:12:37 +0800

    @wordway/translate-webengine-youdao@0.1.4

--- packages/translate-webengine-youdao/src/YoudaoWebEngine.ts
+++ packages/translate-webengine-youdao/src/YoudaoWebEngine.ts
@@ -15,38 +15,33 @@ class YoudaoWebEngine extends TranslateEngine {
     return ['word', 'phrase', 'sentence'];
   }
 
-  lookUp(
+  async lookUp(
     q: string,
     options?: LookUpOptions | undefined
   ): Promise<LookUpResult> {
-    return new Promise((resolve, reject) => {
-      const successCallback = async (response: any) => {
-        const originData = await response.text();
+    const fetch = Translate.overrides.fetch;
 
-        let lookUpResult: LookUpResult = toLookUpResult(
-          this,
-          q,
-          options,
-          originData
-        );
+    let lookUpResult: LookUpResult;
+    try {
+      const r1 = await fetch(
+        `https://dict.youdao.com/w/${encodeURIComponent(q)}`
+      );
+      lookUpResult = toLookUpResult(this, q, options, await r1.text());
 
-        const resp = await Translate.overrides.fetch(
+      try {
+        const r2 = await fetch(
           `https://picdict.youdao.com/search?le=en&q=${encodeURIComponent(q)}`
         );
+        const r2json = await r2.json();
+        lookUpResult.images = r2json?.data?.pic?.map((e: any) => e.url);
+      } catch (_) {
+        // skip
+      }
+    } catch (error) {
+      throw error;
+    }
 
-        lookUpResult.images =
-          JSON.parse(await resp.text())?.data?.pic?.map((e: any) => e.url) ||
-          [];
-
-        resolve(lookUpResult);
-      };
-      const failureCallback = (error: any) => reject(error);
-
-      Translate.overrides
-        .fetch(`https://dict.youdao.com/w/${encodeURIComponent(q)}`)
-        .then(successCallback)
-        .catch(failureCallback);
-    });
+    return lookUpResult;
   }
 }
 
